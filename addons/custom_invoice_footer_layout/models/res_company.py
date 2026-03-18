186e199e71ca4b18d7bb862a822fe330426a173f
JustelioCode <krutikovasjustinas@gmail.com>
2026-03-18 14:11:50 +0200

    update of layout

--- addons/custom_invoice_footer_layout/models/res_company.py
+++ addons/custom_invoice_footer_layout/models/res_company.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    invoice_footer_col_1 = fields.Text(string="Invoice Footer Column 1")
-    invoice_footer_col_2 = fields.Text(string="Invoice Footer Column 2")
-    invoice_footer_col_3 = fields.Text(string="Invoice Footer Column 3")
+    invoice_footer_col_1 = fields.Html(string="Invoice Footer Column 1", sanitize=False)
+    invoice_footer_col_2 = fields.Html(string="Invoice Footer Column 2", sanitize=False)
+    invoice_footer_col_3 = fields.Html(string="Invoice Footer Column 3", sanitize=False)
